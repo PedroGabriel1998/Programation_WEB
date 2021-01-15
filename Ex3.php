ca60089da864957ecd989629586b4bf32477450e
PedroGabriel1998 <pedro.pgs1998@gmail.com>
2021-01-15 19:08:33 -0300

    subindo arquivos

--- Ex3.php
+++ Ex3.php
@@ -2,9 +2,34 @@
 <html> 
   
 <head> 
+    <link href = "EstiloDropDown.css" rel = "stylesheet" type = "text/css"> 
+    <meta charset="UTF-8"/>
+    <title>Semana 05 - EX3</title>
 </head> 
   
 <body> 
+    <nav>
+        <p>Pedro Gabriel</p>
+        <ul>
+            <li><a href = "index.html">Home</a></li>
+                <li><a href = "#">Programação WEB</a>
+                <ul>
+                    <li><a href = "./Sem1.html">Semana 01</a></li>
+                    <li><a href = "./Sem2.html">Semana 02</a></li>
+                    <li><a href = "./Sem3.html">Semana 03</a></li>
+                    <li><a href = "./Sem4.html">Semana 04</a></li>
+                    <li><a href = "./Sem5.html">Semana 05</a></li>
+                </ul>
+                </li>
+                <li><a href = "#">TCC Senai 2020</a>
+                    <ul>
+                        <li><a href = "./BracoRobotico.html">Braço Robótico</a></li>
+                        <li><a href = "#">Eletroímã</a></li>
+                        <li><a href = "#">Mesa de Sensores</a></li>
+                    </ul>
+                </li>
+        </ul>
+    </nav>
     <?php 
         // Defining variables 
         $B_Maior = $B_Menor = $Altura = $area = $LBase = $AlturaP = $volume = 0; 
